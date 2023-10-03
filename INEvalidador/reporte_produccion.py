cc65514823be717291f9d747ee83b4794b908262
MariajoseChinchilla <mchinchimoran@gmail.com>
2023-10-03 11:38:51 -0600

    Revert "a"
    This reverts commit f66d6aaaaf5a7ec0892728290e8fe67eddea07a2.

--- INEvalidador/reporte_produccion.py
+++ INEvalidador/reporte_produccion.py
@@ -4,9 +4,6 @@ import os
 from datetime import datetime
 import time
 import matplotlib as plt
-from INEvalidador.conexionSQL import baseSQL
-sql = baseSQL(False)
-import pandas as pd
 
 class GestorConteos:
     def __init__(self):
@@ -16,49 +13,6 @@ class GestorConteos:
         gauth.LocalWebserverAuth()
         self.drive = GoogleDrive(gauth)
         self.FOLDER_ID = '1xC2oCDbSiVCRVeilCK7SLyRhNCdQ8yaY'
-
-    def escribir_query_sql(self, archivo, nombre_analista):
-        folder_path = os.path.join(os.getcwd(), nombre_analista)
-        nombre_sintaxis = archivo.replace(".xlsx", "")
-        
-        # Crea el directorio si aún no existe
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-        
-        df_queries = pd.read_excel(archivo)
-        now = datetime.now()
-        date_str = now.strftime("%d-%m-%Y")
-        
-        # Modifica la ruta de la variable sintaxis
-        sintaxis = os.path.join(folder_path, f"{nombre_sintaxis}_{date_str}.txt")
-        # Iniciar ciclo de escritura de las sintaxis SQL
-        vars = list(df_queries["variable"])
-        tablas = [sql.base_col.get(i.split(".", 1)[1].upper()) for i in vars if "." in i]
-        valores_nuevos = list(df_queries["valor nuevo"])
-        ronda = [f"ENCOVI_{tablas[0][-2:]}"] * len(tablas)
-        vars = [var.split(".", 1)[1] for var in vars]
-        tablas = [tabla[:-3] for tabla in tablas]
-        datos_cart = list(df_queries[["depto","mupio","sector","estructura","vivienda","hogar","cp"]].itertuples(index=False))
-        filtros = []
-        for dep, mup, sec, estr, viv, hog, cpp in datos_cart:
-            filtros.append(f"'level-1'.depto = {dep} and 'level-1'.mupio = {mup} and 'level-1'.sector = {sec} and 'level-1'.estructura = {estr} and 'level-1'.vivienda = {viv} and 'level-1'.hogar = {hog} and 'level-1'.cp = {cpp}")
-        cuadruplas = list(zip(ronda, tablas, vars, valores_nuevos, filtros))
-        for rond, tabla, variable, valor_nuevo, filtro in cuadruplas:
-            with open(sintaxis, "a") as archivo:
-                archivo.write(f"UPDATE {rond}.{tabla} AS {tabla} SET {tabla}.{variable} = {valor_nuevo} WHERE {filtro}; \n")
-        for rond, tabla, variable, valor_nuevo, filtro in cuadruplas:
-            # Reemplazar la subcadena " and 'level-1'.cp = 0" por la cadena vacía
-            filtro = filtro.replace(" and 'level-1'.cp = 0", "")
-            with open(sintaxis, "a") as archivo:
-                archivo.write(f"UPDATE {rond}.{tabla} AS {tabla} SET {tabla}.{variable} = {valor_nuevo} WHERE {filtro}; \n")  # \n para una nueva línea después de cada instrucción
-
-        with open(sintaxis, 'r') as archivo:
-            contenido = archivo.read()
-            contenido = contenido.replace(" and 'level-1'.cp = 0", "")
-
-        with open(sintaxis, 'w') as archivo:
-            archivo.write(contenido)
-
         
     def inicializar_archivo(self):
         """Crea y sube un archivo CSV inicial para los conteos de analistas a Google Drive."""
@@ -68,13 +22,15 @@ class GestorConteos:
         file_drive.SetContentFile('conteo_analistas.csv')
         file_drive.Upload()
 
-    def obtener_conteo_desde_archivo(self, analista, ruta_archivo):
-            """Devuelve el conteo de líneas desde el archivo del analista."""
-            if not os.path.exists(ruta_archivo):
-                return 0
-            with open(ruta_archivo, 'r') as file:
-                return sum(1 for _ in file)
-
+    def obtener_conteo_desde_archivo(self, analista):
+        """Devuelve el conteo de líneas desde el archivo del analista del día actual."""
+        fecha_actual = datetime.now().strftime('%Y-%m-%d')
+        nombre_carpeta = f"{analista}_{fecha_actual}"
+        ruta_archivo = os.path.join(os.path.expanduser("~"), "Desktop", nombre_carpeta, f"{analista}.txt")
+        if not os.path.exists(ruta_archivo):
+            return 0
+        with open(ruta_archivo, 'r') as file:
+            return sum(1 for _ in file)
 
     def actualizar_conteo(self, analista):
         """Actualiza el conteo del analista en el archivo CSV en Google Drive."""
@@ -158,12 +114,4 @@ class GestorConteos:
         plt.savefig(ruta_imagen)
 
         # Mostrar el histograma (opcional)
-        plt.show()
-
-    def procesar_analista(self, analista, ruta_archivo):
-            """Procesa el archivo del analista y actualiza el conteo en Drive."""
-            # Obtener conteo de líneas del archivo del analista
-            self.obtener_conteo_desde_archivo(analista, ruta_archivo)
-
-            # Actualizar el conteo del analista en el archivo CSV en Google Drive
-            self.actualizar_conteo_con_bloqueo(analista)
+        # plt.show()
