c0667b534ea3086bf0db53fb114f598d847687d0
MariajoseChinchilla <mchinchimoran@gmail.com>
2023-10-13 21:20:47 -0600

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='INEvalidador',
-    version='1.4.1',
+    version='1.4.2',
     author='Mariajose Chinchilla Moran',
     description='Validador de datos de criterios de encuestas estilo INE.',
     long_description='',
