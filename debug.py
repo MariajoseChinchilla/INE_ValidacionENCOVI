25b8212548236ac472689adfba36a1c631125ffc
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-10-19 09:59:00 -0600

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,5 +1,4 @@
 from INEvalidador.limpieza import Limpieza
-limpieza = Limpieza(1,descargar=False)
 
-limpieza.escribir_query_sq(archivo=r"C:\Users\mchinchilla\Desktop\Validador\Datos para Revisión\output_17-10-2023\Prueba Majo.xlsx",comision=1,usuario="mchinchilla")
-#limpieza.df_para_limpieza("'DEPTO = 12 & MUPIO = 15 & SECTOR = 14649 & ESTRUCTURA = 133 & VIVIENDA = 121 & HOGAR = 4 '")
+limpieza = Limpieza(1, "", False, "3307", "3307", "mchinchilla", "Mchinchilla2023")
+limpieza.limpieza_por_query("a", "PPA03 es vacio", "PPA03")
